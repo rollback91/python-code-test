5987409cfe00f52dce26d5cc415eb4eb6852aa25
Rollback <moccimarco@gmail.com>
2017-12-03 23:23:45 +0100

    Add logic to display reactions for a give episode
    For each episode we need to render the reactions related
    to it.
    Only the not deleted reactions should be listed.
    Added a little style with bootstrap.

--- testsite/episodes/views.py
+++ testsite/episodes/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from .models import Episode
+from itertools import chain
+from operator import attrgetter
 # Create your views here.
+
+
+def show_reactions(request, id):
+    episode = get_object_or_404(Episode, pk=id)
+    img_reactions = episode.imagereaction_set.all().filter(deleted=False)
+    tweet_reactions = episode.tweetreaction_set.all().filter(deleted=False)
+
+    reactions = sorted(chain(img_reactions, tweet_reactions), key=attrgetter('created_at'))
+
+    context = dict(reactions=reactions)
+
+    return render(request, 'episode.html', context=context)

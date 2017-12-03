8e2472b4a4d129c27fd683ef31af64eabccf1620
Rollback <moccimarco@gmail.com>
2017-12-03 23:51:47 +0100

    Add tests for episodes app
    We shall test that if a episode does not exist the response is 404.
    We shall test that if a episode exists the response is 200.
    We shall test that if a episode does not have any reactions somenthing
    will be displayed.

--- testsite/episodes/tests.py
+++ testsite/episodes/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.test import Client
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+def get_episode_rections_response(id):
+    client = Client()
+    url = reverse('episode-reactions', kwargs={'id':id})
+    return client.get(url)
+
+
+class EpisodeTests(TestCase):
+    fixtures = ['test_data.json']
+
+    def test_unexistend_episode_will_return_404(self):
+        response = get_episode_rections_response(999)
+        self.assertEqual(404, response.status_code)
+
+    def test_episode_will_return_200(self):
+        response = get_episode_rections_response(2)
+        self.assertEqual(200, response.status_code)
+
+    def test_episode_with_no_reactions_will_display_somenthing(self):
+        response = get_episode_rections_response(3)
+        self.assertContains(response, 'Nothing ')

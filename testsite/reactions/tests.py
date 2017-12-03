e8cae7bb65999e334585bb85b24783822351334f
Rollback <moccimarco@gmail.com>
2017-12-03 22:50:40 +0100

    Add tests for admin actions

--- testsite/reactions/tests.py
+++ testsite/reactions/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from .models import TweetReaction, ImageReaction
+from django.test import Client
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+def logged_client():
+    client = Client()
+    client.login(username='admin', password='admin')
+    return client
+
+
+class ReactionTests(TestCase):
+    fixtures = ['test_data.json']
+
+    def test_admin_delete_action(self):
+        client = logged_client()
+        reaction = TweetReaction.objects.get(pk=1)
+        self.assertFalse(reaction.deleted)
+
+        url = reverse('admin:reactions_tweetreaction_changelist')
+        data = {'action': 'delete_selected', '_selected_action': '1'}
+        response = client.post(url, data, follow=True)
+
+        reaction = TweetReaction.objects.get(pk=1)
+        self.assertTrue(reaction.deleted)
+
+    def test_admin_udelete_action(self):
+        client = logged_client()
+        reaction = TweetReaction.objects.get(pk=3)
+        self.assertTrue(reaction.deleted)
+
+        url = reverse('admin:reactions_tweetreaction_changelist')
+        data = {'action': 'undelete_selected', '_selected_action': '1'}
+        response = client.post(url, data, follow=True)
+
+        reaction = TweetReaction.objects.get(pk=1)
+        self.assertFalse(reaction.deleted)

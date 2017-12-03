0f45481636afb4200e1524b5b2ede527af7b6e6a
Rollback <moccimarco@gmail.com>
2017-12-03 18:05:13 +0100

    Add create_fixture and load_fixture methods
    Using fabric is now possible to save fixture from apps in the whole
    project.

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,9 @@
+import os
 from fabric.api import local
 
 
-def run_manage(command):
-    local('/home/ubuntu/.virtualenvs/code-test/bin/python /vagrant/testsite/manage.py %s' % command)
+def run_manage(command, capture=False):
+    return local('/home/ubuntu/.virtualenvs/code-test/bin/python /vagrant/testsite/manage.py %s' % command, capture=capture)
 
 
 def web():
@@ -19,3 +20,28 @@ def make_migrations():
 
 def requirements():
     local('/home/ubuntu/.virtualenvs/code-test/bin/pip install -r requirements.txt ')
+
+
+def _save_fixture(app, fixture):
+    directory = './testsite/{}/fixtures/'.format(app)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    with open('{}initial_data.json'.format(directory), 'w') as f:
+        f.writelines(fixture)
+
+
+def _make_fixture(app):
+    fixtures = run_manage('dumpdata {} --indent=4'.format(app), capture=True)
+    _save_fixture(app, fixtures)
+
+
+def create_fixtures():
+    _make_fixture('reactions')
+    _make_fixture('episodes')
+
+
+def load_fixtures():
+    run_manage('loaddata ./testsite/reactions/fixtures/initial_data.json')
+    run_manage('loaddata ./testsite/episodes/fixtures/initial_data.json')
